e11320f694797c14e24677bb3b32db165956b827
ViperCiper <33349529+ViperCiper@users.noreply.github.com>
2017-11-03 16:09:57 -0400

    Update X_Lime.py

--- X_Lime.py
+++ X_Lime.py
@@ -12,7 +12,7 @@ print colored(': ##:. ##:: ##:::::::: ##:: ##:.:: ##: ##:::::::', 'green', attrs
 print colored(' ##:::. ##: ########: ####: ##:::: ##: ########:', 'green', attrs=['bold'])
 print colored('..:::::..::........::....::..:::::..::........::', 'green', attrs=['bold'])
 print colored('====================', 'red', attrs=['bold']), colored('X_LIME', 'blue', attrs=['bold']), colored('====================', 'red', attrs=['bold'])
-print colored('SELECT AN OPTION TO BEGIN: ', 'white')
+print '\033[1;37m[\033[1;m''\033[1;32m--\033[1;m''\033[1;37m]\033[1;m''\033[1;33mSELECT AN OPTION TO BEGIN: \033[1;m'
 print '\033[1;37m[\033[1;m''\033[1;32m1\033[1;m''\033[1;37m]\033[1;m'
 print '\033[1;37m[\033[1;m''\033[1;32m2\033[1;m''\033[1;37m]\033[1;m'
 print '\033[1;37m[\033[1;m''\033[1;32m3\033[1;m''\033[1;37m]\033[1;m'
