bee06c46b73f4e20c47039848db2ad45d7797081
ViperCihper <33349529+ViperCiper@users.noreply.github.com>
2018-01-10 22:31:42 +0300

    Add files via upload

---
+++ X_Lime/config.py
@@ -0,0 +1,10 @@
+import os
+os.system('chmod +x X_Lime/launch_xlime.py')
+
+os.system('apt-get install apktool')
+os.system('sudo apt-get install python-termcolor')
+os.system('pip install pyfiglet')
+os.system('pip install termcolor')
+os.system('pip install colorama')
+os.system('pip install progressbar2')
+os.system('apt-get install dex2jar')
