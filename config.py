4a5785534d587995af4b61d7acbc6f761b1e17c5
Vip3r <33349529+Vip3r-MC@users.noreply.github.com>
2020-01-13 02:17:12 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system('chmod +x X_Lime/launch_xlime.py')
-os.system('sudo apt-get install apktool -y')
-os.system('sudo apt install zipalign -y')
-os.system('sudo apt-get install python-termcolor -y')
-os.system('sudo apt-get install dex2jar -y')
