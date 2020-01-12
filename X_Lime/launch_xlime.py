dba09b67697ac415f2b4d374bee066637255d839
Vip3r <33349529+Vip3r-MC@users.noreply.github.com>
2020-01-13 02:18:04 +0300

    Delete launch_xlime.py

--- X_Lime/launch_xlime.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import os
-import signal
-
-os.system("gnome-terminal --hide-menubar -e 'bash -c \"python Assest/xlime_run.py; exec bash\"'")
-os.kill(os.getppid(), signal.SIGHUP)
